86dbc8e055f94c661563bc17e8be7dbe6fd4d80b
Valchy <valchygaming@gmail.com>
2022-01-12 18:17:26 +0100

    tinkerwell playground + tailwind

---
+++ tinkerwell/users.php
@@ -0,0 +1,25 @@
+<?php
+// Tinkerwell only prints one thing
+// Always prints the last variable
+
+use App\Models\User;
+
+// Get user with id less or equal to 5
+User::query()->where('id', '<=', 5)->get()->pluck('name');
+
+// Get single user with id 2
+// User::query()->where('id', 2)->get();
+
+User::query()->where('id', 1)->orWhere('id', 4)->get();
+
+
+
+
+$articles = collect([
+  ['name' => 'Valeri', 'age' => 20],
+  ['name' => 'Viki', 'age' => 19]
+]);
+
+$articles->map(function($item) {
+  return $item['name'] . ' is ' . $item['age'] . ' years old.';
+});
