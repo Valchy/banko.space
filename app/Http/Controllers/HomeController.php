e7e8898e9cbe92be07b73b93d9180fe74eb53fe2
Valchy <valchygaming@gmail.com>
2022-01-14 12:43:21 +0100

    Home Page Display of Data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,17 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Transaction;
 
 class HomeController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function __invoke(Request $request)
     {
-        return view('home');
+        $transactions = Transaction::all();
+
+        return view('home', compact('transactions'));
     }
 }
