67d5ebf0f6dec41db760c9b225dbfbe108c0ad5e
Valeri Sabev <valchygaming@gmail.com>
2022-01-21 12:38:57 +0100

    testing styleci advanced

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,10 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
-{
-    public function __invoke(Request $request)
-    {
+class HomeController extends Controller {
+    public function __invoke(Request $request) {
         session()->put('session_fancy_name', 'TEST COOOl');
 
         return view('home');
