678945462d67758d1f2a6ff65dbfe2fabbcd1e32
Valeri Sabev <valchygaming@gmail.com>
2022-01-23 01:30:40 +0100

    Is_admin middleware bug fix

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -9,7 +9,7 @@ class IsAdmin
 {
     public function handle(Request $request, Closure $next)
     {
-        if (auth()->check() && auth()->user->is_admin) {
+        if (auth()->check() && auth()->user()->is_admin) {
             return $next($request);
         } else {
             abort(403);
