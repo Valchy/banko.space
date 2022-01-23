35d250b2b8a53ed33c34a2f67c702ac21e9be12e
Valeri Sabev <valchygaming@gmail.com>
2022-01-23 02:01:17 +0100

    Footer improvement - COMPONENT THINKING

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,19 +1,11 @@
 <div class="bg-linear-pink-invert p-12 pt-3 lg:pt-12">
     <div tabindex="0" aria-label="footer" class="focus:outline-none mx-auto container flex flex-col items-center justify-center">
         <div class="text-black flex flex-col md:items-center f-f-l pt-3">
-            <h1 tabindex="0" class="focus:outline-none text-center text-2xl font-black">Send and Receive Money in SECONDS</h1>
-            <div class="md:flex items-center mt-5 md:mt-10 text-base text-color f-f-l">
-                <h2  class=" md:mr-6 pb-4 md:py-0 cursor-pointer"><a  class="focus:outline-none focus:underline hover:text-gray-700" href="javascript:void(0)">Download Now</a> </h2>
-                <h2  class="cursor-pointer"><a class="focus:outline-none focus:underline hover:text-gray-700" href="javascript:void(0)">License</a> </h2>
-            </div>
             <div class="my-6 text-base text-color f-f-l"> 
                 <ul class="md:flex items-center">
-                    <li class="md:mr-6 cursor-pointer pt-4 lg:py-0"><a href="javascript:void(0)" class="focus:outline-none focus:underline hover:text-gray-500">About </a></li>
-                    <li class="md:mr-6 cursor-pointer pt-4 lg:py-0"><a href="javascript:void(0)" class="focus:outline-none focus:underline hover:text-gray-500">Features </a></li>
-                    <li class="md:mr-6 cursor-pointer pt-4 lg:py-0"><a href="javascript:void(0)" class="focus:outline-none focus:underline hover:text-gray-500">Pricing </a></li>
-                    <li class="md:mr-6 cursor-pointer pt-4 lg:py-0"><a href="javascript:void(0)" class="focus:outline-none focus:underline hover:text-gray-500">Careers </a></li>
-                    <li class="md:mr-6 cursor-pointer pt-4 lg:py-0"><a href="javascript:void(0)" class="focus:outline-none focus:underline hover:text-gray-500">Help </a></li>
-                    <li class="cursor-pointer pt-4 lg:py-0"><a href="javascript:void(0)" class="focus:outline-none focus:underline hover:text-gray-500">Privacy Policy </a></li>
+                    <li class="md:mr-6 cursor-pointer pt-4 lg:py-0"><a href="/dashboard" class="focus:outline-none focus:underline hover:text-gray-500">Dashboard </a></li>
+                    <li class="md:mr-6 cursor-pointer pt-4 lg:py-0"><a href="/transaction-history" class="focus:outline-none focus:underline hover:text-gray-500">Transaction History </a></li>
+                    <li class="md:mr-6 cursor-pointer pt-4 lg:py-0"><a href="/download" class="focus:outline-none focus:underline hover:text-gray-500">Download Now </a></li>
                 </ul>
             </div>
             <div class="text-sm text-color mb-10 f-f-l">
@@ -22,16 +14,12 @@
         </div>
         <div class="w-9/12 h-0.5 bg-gray-100 rounded-full"></div>
         <div class="flex justify-between items-center pt-12">
-            <a href="javascript:void(0)" class="hover:shadow-lg mr-4 focus:outline-none rounded-md focus:ring-2 focus:ring-offset-2 focus:ring-gray-600" aria-label="download on the app store">
-                <div class="">
-                    <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/simple_center_aligned_with_logo-svg2.svg" alt="download on the app store">
-                </div>
-            </a>
-            <button  class="hover:shadow-lg focus:outline-none rounded-md focus:ring-2 focus:ring-offset-2 focus:ring-gray-600" aria-label="get it on google play"></a>
-            <div>
-              <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/simple_center_aligned_with_logo-svg3.svg" alt="get it on google play">                    
-            </div>
-            </button>
+            <x-footer.download-btn>
+                <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/simple_center_aligned_with_logo-svg2.svg" alt="download on the app store">
+            </x-footer.download-btn>
+            <x-footer.download-btn>
+                <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/simple_center_aligned_with_logo-svg3.svg" alt="get it on google play">
+            </x-footer.download-btn>
         </div>
     </div>
 </div>
---
+++ resources/views/components/footer/download-btn.blade.php
@@ -0,0 +1,5 @@
+<a download href="/download" class="hover:shadow-lg mr-4 focus:outline-none rounded-md focus:ring-2 focus:ring-offset-2 focus:ring-gray-600" aria-label="download on the app store">
+    <div class="">
+        {{$slot}}
+    </div>
+</a>
