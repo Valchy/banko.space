9fbb8be2f3a3ecf30e3609f5b5caf3fc68d17875
Valeri Sabev <valchygaming@gmail.com>
2022-01-24 12:27:07 +0100

    Livewire first commit

---
+++ app/Http/Livewire/TransactionData.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class TransactionData extends Component
+{
+    public function render()
+    {
+        return view('livewire.transaction-data');
+    }
+}
