1913c4c24b04d1ba9760ca6eded87a4c1527c5b0
Valchy <valchygaming@gmail.com>
2022-01-25 10:42:29 +0100

    Health Check Service Provider

--- app/Providers/HealthCheckServiceProvider.php
+++ app/Providers/HealthCheckServiceProvider.php
@@ -3,12 +3,15 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Spatie\Health\Facades\Health;
 use Spatie\Health\Checks\Checks\CacheCheck;
 use Spatie\Health\Checks\Checks\DatabaseCheck;
 use Spatie\Health\Checks\Checks\UsedDiskSpaceCheck;
-use Spatie\Health\Facades\Health;
+use Spatie\Health\Checks\Checks\DebugModeCheck;
+use Spatie\Health\Checks\Checks\EnvironmentCheck;
+use Spatie\Health\Checks\Checks\PingCheck;
 
-class HealthCheck extends ServiceProvider
+class HealthCheckServiceProvider extends ServiceProvider
 {
     public function register()
     {
@@ -21,6 +24,9 @@ class HealthCheck extends ServiceProvider
             CacheCheck::new(),
             UsedDiskSpaceCheck::new(),
             DatabaseCheck::new(),
+            DebugModeCheck::new(),
+            EnvironmentCheck::new(),
+            PingCheck::new()->url('https://valeri.harbourspace.site')->timeout(30),
         ]);
     }
 }
--- config/app.php
+++ config/app.php
@@ -176,7 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         // Health Check Provider
-        App\Providers\HealthCheck::class,
+        App\Providers\HealthCheckServiceProvider::class,
     ],
 
     /*
