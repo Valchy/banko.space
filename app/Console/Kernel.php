1eb8527f00e2b03137a67a1973d4fcea3147acf4
Valchy <valchygaming@gmail.com>
2022-01-25 10:14:14 +0100

    Schedule Health Checks

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,6 +17,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command(\Spatie\Health\Commands\RunHealthChecksCommand::class)->everyMinute();
     }
 
     /**
