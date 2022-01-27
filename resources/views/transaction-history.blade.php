04f831e72b85a51beb456fc1af9965d68729abd2
Valeri Sabev <valchygaming@gmail.com>
2022-01-27 02:23:44 +0100

    Update transaction-history.blade.php

--- resources/views/transaction-history.blade.php
+++ resources/views/transaction-history.blade.php
@@ -1,14 +1,10 @@
 <x-site-layout title="Banko.Space">
 	<center>
-		<br/>
-		<br/>
-		<span>Welcome to the bank of the future.</span>
-		<br/>
-		<br/>
-		<br/>
-
 		<h1 class="pb-3 text-2xl font-black">Transactions</h1>
 		<livewire:transaction-search/>
+
+		<br/>
+		<br/>
 
 		<x-transaction-table :transactions="$transactions"/>
 		{{$transactions->links()}}
