3dd96b369bc8cf9fadeabebc21473e6c30312612
Valchy <valchygaming@gmail.com>
2022-01-27 19:38:35 +0100

    Language

--- resources/views/livewire/page-language.blade.php
+++ resources/views/livewire/page-language.blade.php
@@ -1,10 +1,5 @@
-<div class="flex align-center">
-    <div wire:click="changePageLanguage('en')" class="cursor-pointer flex-flex-col justify-center align-center m-4">
-        <img class="border-2 rounded-full {{app()->getLocale() == 'en' ? 'border-black' : ''}}" width="23" src="{{asset('imgs/en_flag.png')}}" alt="english flag">
-        <span class="{{app()->getLocale() == 'en' ? 'font-semibold' : ''}}">EN</span>
-    </div>
-    <div wire:click="changePageLanguage('bg')" class="cursor-pointer flex-flex-col justify-center align-center m-4">
-        <img class="border-2 rounded-full {{app()->getLocale() == 'bg' ? 'border-black' : ''}}" width="23" src="{{asset('imgs/bg_flag.png')}}" alt="bulgarian flag">
-        <span class="{{app()->getLocale() == 'bg' ? 'font-semibold' : ''}}">BG</span>
-    </div>
+<div class="flex align-center my-5 mx-3">
+    <span wire:click="changePageLanguage('en')" class="cursor-pointer {{app()->getLocale() == 'en' ? 'font-semibold' : ''}}">EN</span>
+    <span class="mx-2">/</span>
+    <span wire:click="changePageLanguage('bg')" class="cursor-pointer {{app()->getLocale() == 'bg' ? 'font-semibold' : ''}}">БГ</span>
 </div>
