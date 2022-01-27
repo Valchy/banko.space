811ca57edba15e31c63feb1c7828079aab0289be
Valeri Sabev <valchygaming@gmail.com>
2022-01-27 11:58:38 +0100

    download app file

--- resources/views/components/footer/download-btn.blade.php
+++ resources/views/components/footer/download-btn.blade.php
@@ -1,4 +1,4 @@
-<a download href="/download" class="hover:shadow-lg mr-4 focus:outline-none rounded-md focus:ring-2 focus:ring-offset-2 focus:ring-gray-600" aria-label="download on the app store">
+<a download href="/download.txt" class="hover:shadow-lg mr-4 focus:outline-none rounded-md focus:ring-2 focus:ring-offset-2 focus:ring-gray-600" aria-label="download on the app store">
     <div class="">
         <img src="{{$src}}" alt="{{$alt}}"/>
     </div>
