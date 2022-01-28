dcf464e61c9fa140062945640291ac909ccd5f25
Valeri Sabev <valchygaming@gmail.com>
2022-01-28 05:50:19 +0100

    Update InitialSeed.php

--- app/Console/Commands/InitialSeed.php
+++ app/Console/Commands/InitialSeed.php
@@ -19,22 +19,22 @@ class InitialSeed extends Command
 
     public function handle()
     {
-        $customers = collect([                                                                     // Hey github check out this awesome password :P
-            ['username' => 'Valeri Sabev', 'name' => 'Valeri Sabev', 'is_admin' => true, 'email' => 'valerisabev.com@gmail.com', 'password' => 'coolpass'],
-            ['username' => 'Valchy', 'name' => 'Valchy', 'is_admin' => true, 'email' => 'valchygaming@gmail.com', 'password' => 'coolpass'],
-            ['username' => 'Marchoo', 'name' => 'Mario G', 'is_admin' => true, 'email' => 'mario@bankospace.com', 'password' => 'tisigay'],
-            ['username' => 'Adel01', 'name' => 'Adel', 'is_admin' => false, 'email' => 'adel@bankospace.com', 'password' => 'password'],
-            ['username' => 'Anier8', 'name' => 'Anier', 'is_admin' => false, 'email' => 'anier@bankospace.com', 'password' => 'password'],
-            ['username' => 'Ariel2', 'name' => 'Ariel', 'is_admin' => false, 'email' => 'ariel@bankospace.com', 'password' => 'password'],
-            ['username' => 'Israa333', 'name' => 'Israa', 'is_admin' => false, 'email' => 'israa@bankospace.com', 'password' => 'password'],
-            ['username' => 'Mathias7', 'name' => 'Mathias', 'is_admin' => false, 'email' => 'mathias@bankospace.com', 'password' => 'password'],
-            ['username' => 'Mazen11', 'name' => 'Mazen', 'is_admin' => false, 'email' => 'mazen@bankospace.com', 'password' => 'password'],
-            ['username' => 'Miguel5', 'name' => 'Miguel', 'is_admin' => false, 'email' => 'miguel@bankospace.com', 'password' => 'password'],
-            ['username' => 'Nika6', 'name' => 'Nika', 'is_admin' => false, 'email' => 'nika@bankospace.com', 'password' => 'password'],
-            ['username' => 'Reham4', 'name' => 'Reham', 'is_admin' => false, 'email' => 'reham@bankospace.com', 'password' => 'password'],
-            ['username' => 'Umaima23', 'name' => 'Umaima', 'is_admin' => false, 'email' => 'umaima@bankospace.com', 'password' => 'password'],
-            ['username' => 'Victor98', 'name' => 'Victor', 'is_admin' => false, 'email' => 'victor@bankospace.com', 'password' => 'password'],
-            ['username' => 'Yuri123', 'name' => 'Yuri', 'is_admin' => false, 'email' => 'yuri@bankospace.com', 'password' => 'password'],
+        $customers = collect([
+            ['username' => 'Valeri Sabev', 'name' => 'Valeri Sabev', 'is_admin' => true, 'email' => 'valerisabev.com@gmail.com'],
+            ['username' => 'Valchy', 'name' => 'Valchy', 'is_admin' => true, 'email' => 'valchygaming@gmail.com'],
+            ['username' => 'Marchoo', 'name' => 'Mario G', 'is_admin' => true, 'email' => 'mario@bankospace.com'],
+            ['username' => 'Adel01', 'name' => 'Adel', 'is_admin' => false, 'email' => 'adel@bankospace.com'],
+            ['username' => 'Anier8', 'name' => 'Anier', 'is_admin' => false, 'email' => 'anier@bankospace.com'],
+            ['username' => 'Ariel2', 'name' => 'Ariel', 'is_admin' => false, 'email' => 'ariel@bankospace.com'],
+            ['username' => 'Israa333', 'name' => 'Israa', 'is_admin' => false, 'email' => 'israa@bankospace.com'],
+            ['username' => 'Mathias7', 'name' => 'Mathias', 'is_admin' => false, 'email' => 'mathias@bankospace.com'],
+            ['username' => 'Mazen11', 'name' => 'Mazen', 'is_admin' => false, 'email' => 'mazen@bankospace.com'],
+            ['username' => 'Miguel5', 'name' => 'Miguel', 'is_admin' => false, 'email' => 'miguel@bankospace.com'],
+            ['username' => 'Nika6', 'name' => 'Nika', 'is_admin' => false, 'email' => 'nika@bankospace.com'],
+            ['username' => 'Reham4', 'name' => 'Reham', 'is_admin' => false, 'email' => 'reham@bankospace.com'],
+            ['username' => 'Umaima23', 'name' => 'Umaima', 'is_admin' => false, 'email' => 'umaima@bankospace.com'],
+            ['username' => 'Victor98', 'name' => 'Victor', 'is_admin' => false, 'email' => 'victor@bankospace.com'],
+            ['username' => 'Yuri123', 'name' => 'Yuri', 'is_admin' => false, 'email' => 'yuri@bankospace.com'],
         ]);
 
         foreach ($customers as $customer) {
@@ -46,7 +46,7 @@ class InitialSeed extends Command
             $user = User::create([
                 'name'       => $customer['name'],
                 'email'      => $customer['email'],
-                'password'   => Hash::make($customer['password']),
+                'password'   => Hash::make('password'),
                 'account_id' => $newAccount->id,
                 'is_admin'   => $customer['is_admin'],
             ]);
