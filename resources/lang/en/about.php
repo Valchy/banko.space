ab761752b357da4d8af6c4ccb7fa4c48ec43a4ed
Valeri Sabev <valchygaming@gmail.com>
2022-01-28 01:04:15 +0100

    Home Page About Us Section

---
+++ resources/lang/en/about.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'promotion'=> 'Sign up NOW and receive $50 for free!',
+    'text'     => 'Banko.Space is the newest digital bank in Europe with a fast growing customer base reaching over 500K users in just 3 months. We specilise in extremely fast transactions with minimal to no fees. Our target is to reach an evaluation of 100M by the end of 2022 and to have over a million <i>active</i> users.'
+];
